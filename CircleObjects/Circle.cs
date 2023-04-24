091406a2097319e8319f943a50ccfa4d4afd2638
Mikael Addae <mikael.addae@gmail.com>
2023-04-24 08:38:31 -0400

    Circle Objects Lab

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+        public double Diameter { get; set; }
+        public double Circumference { get; set; }
+        public double Area { get; set; }
+        public double Growth { get; set; }
+        
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+            this.Diameter = CalculateDiameter(radius);
+            this.Circumference = CalculateCircumference(radius);
+            this.Area = CalculateArea(radius);
+            this.Growth = radius * 2;
+            
+        }
+
+        public static double CalculateDiameter (double radius)
+        {
+            return radius * radius;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+
+
+        public double GetRadius(double radius)
+        {
+            return radius;
+        }
+
+    }
+    
+        
+
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace CircleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius = 0;
+            bool goOn = true;
+
+            Console.WriteLine("Welcome to the circle creator");
+            Console.WriteLine("Please enter a radius");
+            try
+            {
+                radius = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please try again, only numbers please");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong, lets try again");
+            }
+            do
+            {
+                Circle circe = new Circle(radius);
+                Console.WriteLine("Diameter: " + circe.Diameter);
+                Console.WriteLine("Circumference: " + circe.Circumference);
+                Console.WriteLine("Area: " + circe.Area);
+                goOn = Grow();
+                if (goOn == true)
+                {
+                    radius = circe.Growth;
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye, final radius was: " + radius);
+                }
+
+            }
+            while (true);
+            {
+                
+            }
+
+                
+
+
+        }
+        public static bool Grow()
+        {
+            Console.WriteLine("Would you like the circle to grow?  y/n ");
+            string input = Console.ReadLine().ToLower().Trim();
+
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Type \"y\" to grow the circle, type \"n\" to stop");
+                return Grow();
+            }
+        }
+
+    }
+}
